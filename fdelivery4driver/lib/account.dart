7eb9defe4d4614c006e086fa52741fa7d0e7faeb
ngoclyo <phamngoclyo2k@gmail.com>
2021-05-18 09:56:03 +0700

    fix

--- fdelivery4driver/lib/account.dart
+++ fdelivery4driver/lib/account.dart
@@ -20,22 +20,48 @@ class Account extends StatelessWidget {
               restorationId: 'cards_demo_list_view',
               padding: const EdgeInsets.only(top: 8, left: 8, right: 8),
               children: [
+                ListTile(
+                  title: Text("Xin chào Nguyễn VĂn A"),
+                  trailing: PopupMenuButton<String>(
+                    padding: EdgeInsets.zero,
+                    itemBuilder: (context) => <PopupMenuItem<String>>[
+                      PopupMenuItem<String>(
+                        value: 'abc',
+                        child: Text(
+                          'Chỉnh sửa thông tin',
+                        ),
+                      ),
+                      PopupMenuItem<String>(
+                        child: Text(
+                          'Đổi mật khẩu',
+                        ),
+                      ),
+                      PopupMenuItem<String>(
+                        value:
+                        'abc',
+                        enabled: false,
+                        child: Text(
+                          'Vô hiệu hóa tài khoản',
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
                 SizedBox(
                   height: 200,
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
-                      Text(
-                        "Xin chào Nguyễn VĂn A"
-                      ),
                       Image.asset(
                         "assets/logo-bg.png",
                         width: size.width * 0.25,
                       ),
+                      Text("Tôi")
                     ],
                   ),
                 ),
-                SizedBox(height: size.height * 0.05),
+
+                // SizedBox(height: size.height * 0.05),
                 SizedBox(height: size.height * 0.03),
                 SizedBox(
                   height: 50,
@@ -89,7 +115,7 @@ class Account extends StatelessWidget {
                                       mainAxisAlignment: MainAxisAlignment.start,
                                       children: <Widget>[
                                         Text("  "),
-                                        Icon(Icons.add_ic_call_outlined),
+                                        Icon(Icons.work_rounded),
                                         Text("  Thanh toán"),
                                       ]
                                   ),
@@ -125,7 +151,7 @@ class Account extends StatelessWidget {
                                       mainAxisAlignment: MainAxisAlignment.start,
                                       children: <Widget>[
                                         Text("  "),
-                                        Icon(Icons.zoom_out_sharp),
+                                        Icon(Icons.zoom_in_rounded),
                                         Text("  Trung tâm hỗ trợ"),
                                       ]
                                   ),
@@ -161,7 +187,7 @@ class Account extends StatelessWidget {
                                       mainAxisAlignment: MainAxisAlignment.start,
                                       children: <Widget>[
                                         Text("  "),
-                                        Icon(Icons.add_ic_call_outlined),
+                                        Icon(Icons.account_balance_wallet_rounded),
                                         Text("  Ứng dụng của khách hàng"),
                                       ]
                                   ),
@@ -197,7 +223,7 @@ class Account extends StatelessWidget {
                                       mainAxisAlignment: MainAxisAlignment.start,
                                       children: <Widget>[
                                         Text("  "),
-                                        Icon(Icons.add_ic_call_outlined),
+                                        Icon(Icons.announcement_rounded),
                                         Text("  Chính sách quy định"),
                                       ]
                                   ),
@@ -233,7 +259,7 @@ class Account extends StatelessWidget {
                                       mainAxisAlignment: MainAxisAlignment.start,
                                       children: <Widget>[
                                         Text("  "),
-                                        Icon(Icons.add_ic_call_outlined),
+                                        Icon(Icons.ac_unit_rounded),
                                         Text("  Cài đặt"),
                                       ]
                                   ),
@@ -269,7 +295,7 @@ class Account extends StatelessWidget {
                                       mainAxisAlignment: MainAxisAlignment.start,
                                       children: <Widget>[
                                         Text("  "),
-                                        Icon(Icons.add_ic_call_outlined),
+                                        Icon(Icons.workspaces_filled),
                                         Text("  Thông tin ứng dụng"),
                                       ]
                                   ),
@@ -289,8 +315,6 @@ class Account extends StatelessWidget {
                     ),
                   ),
                 ),
-
-
                 Container(
                   margin: EdgeInsets.symmetric(vertical: 10),
                   width: size.width * 0.28,
@@ -321,4 +345,5 @@ class Account extends StatelessWidget {
     );
   }
 }
+
 
